c8c6dab4c34eac9215edee8e719d51aec8208074
Liliana Weimer <lilianaweimer11@gmail.com>
2020-07-16 11:04:14 -0600

    Add some styling

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -29,7 +29,7 @@ class Form extends React.Component {
 
     render() {
         return (
-            <form onSubmit={(event) => this.handleSubmit(event)}>
+            <form className='form' onSubmit={(event) => this.handleSubmit(event)}>
                 <input 
                     type='text' 
                     placeholder='Name' 
