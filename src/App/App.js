13bb4606c1cd39fa0a22c669e2131d5339049963
Liliana Weimer <lilianaweimer11@gmail.com>
2020-07-16 09:26:32 -0600

    Display reservations on the DOM

--- src/App/App.js
+++ src/App/App.js
@@ -28,7 +28,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          <Reservations />
+          <Reservations reservations={this.state.reservations}/>
         </div>
       </div>
     )
--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-function Reservations() {
-    return (<p>reservations!</p>)
+function Reservations(props) {
+    return (
+        props.reservations.map(reservation => {
+            return (<article className='reservation' id='reservation.id' key='reservation.id'>
+                <h3>{reservation.name}</h3>
+                <p>{reservation.date}</p>
+                <p>{reservation.time}</p>
+                <p>Guests: {reservation.number}</p>
+            </article>)
+        })
+    )
 }
 
 export default Reservations;
