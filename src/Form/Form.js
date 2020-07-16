1c55d7a77124ab472dadd0db096047b7554740dd
Liliana Weimer <lilianaweimer11@gmail.com>
2020-07-16 09:39:47 -0600

    Attempt to make form controlled

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
 class Form extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            name: null,
+            date: null,
+            time: null,
+            number: null,
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
 
     render() {
         return (
-            <form onSubmit={this.props.submitReservation}>
-                <input type='text' placeholder='Name' value='this.state.name' name='name'/>
-                <input type='text' placeholder='Date (mm/dd)' value='this.state.date' name='date'/>
-                <input type='text' placeholder='Time' value='this.state.time' name='time'/>
-                <input type='text' placeholder='Number of Guests' value='this.state.number' name='number'/>
+            <form onSubmit={this.handleSubmit}>
+                <input type='text' placeholder='Name' value={this.state.name} name='name' onChange={this.handleChange}/>
+                <input type='text' placeholder='Date (mm/dd)' value={this.state.date} name='date' onChange={this.handleChange}/>
+                <input type='text' placeholder='Time' value={this.state.time} name='time' onChange={this.handleChange}/>
+                <input type='text' placeholder='Number of Guests' value={this.state.number} name='number' onChange={this.handleChange}/>
                 <button>Make Reservation</button>
             </form>
         )
--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Reservations(props) {
     return (
         props.reservations.map(reservation => {
-            return (<article className='reservation' id='reservation.id' key='reservation.id'>
+            return (<article className='reservation' id='reservation.id'>
                 <h3>{reservation.name}</h3>
                 <p>{reservation.date}</p>
                 <p>{reservation.time}</p>
