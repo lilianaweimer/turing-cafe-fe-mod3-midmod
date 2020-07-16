5a4a7339b4a6091beb1c90d26d56220130a56cdf
Liliana Weimer <lilianaweimer11@gmail.com>
2020-07-16 09:32:21 -0600

    Make basic form

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -3,8 +3,16 @@ import React from 'react';
 class Form extends React.Component {
 
     render() {
-        return (<p>FORM!</p>)
+        return (
+            <form onSubmit={this.props.submitReservation}>
+                <input type='text' placeholder='Name' value='this.state.name' name='name'/>
+                <input type='text' placeholder='Date (mm/dd)' value='this.state.date' name='date'/>
+                <input type='text' placeholder='Time' value='this.state.time' name='time'/>
+                <input type='text' placeholder='Number of Guests' value='this.state.number' name='number'/>
+                <button>Make Reservation</button>
+            </form>
+        )
     }
 }
 
-export default FormData;
+export default Form;
